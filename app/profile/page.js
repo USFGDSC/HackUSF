cc75f7c34ab854b593bbab07f2018a510101bd93
Jaehoonoo <justjhoon@gmail.com>
2025-01-30 19:06:06 -0500

    Profile page linked with homepage

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,9 +1,34 @@
 "use client"
 
+import { Box, Typography, Button } from "@mui/material";
+import { UserButton } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+
 export default function Profile() {
-    return (
-        <div>
-            <h1>Profile</h1>
-        </div>
-    )
+  const router = useRouter();
+
+  return (
+    <Box sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: '1rem',
+        height: '100vh',
+    }}>
+      <UserButton 
+        appearance={{
+          elements: {
+            avatarBox: {
+              width: 120,
+              height: 120,
+            }
+          },
+        }} 
+      />
+
+      <Button variant="outlined" onClick={()=> router.push("/")}>Back to Homepage</Button>
+
+    </Box>
+  )
 }
